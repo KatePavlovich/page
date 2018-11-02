e0dfd626afa1faa231c27d851afac651357e03e5
KatePavlovich <asylumhere@mail.ru>
2018-11-02 11:14:26 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio-with-react/precache-manifest.145329e0f11d2ae4988aa0047b725494.js"
+  "/portfolio-with-react/precache-manifest.cfb031b42b34aecc024ec3c922f0961c.js"
 );
 
 workbox.clientsClaim();

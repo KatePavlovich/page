28e6f6915c260f81a9773d364c0ca5e37beb5638
KatePavlovich <asylumhere@mail.ru>
2019-08-25 18:32:38 +0300

    Updates

---
+++ precache-manifest.69eb893cafbbc244ae9112c5b0b18f3f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a06a2f0685d7669e6aec0c116daff04",
+    "url": "/portfolio-with-react/static/media/phone.9a06a2f0.svg"
+  },
+  {
+    "revision": "59cf5ce2530d9e9cc7c7b309ac0eee08",
+    "url": "/portfolio-with-react/static/media/linkedin.59cf5ce2.svg"
+  },
+  {
+    "revision": "e9666b39fe698d868cf73efa8ae05b46",
+    "url": "/portfolio-with-react/static/media/github.e9666b39.svg"
+  },
+  {
+    "revision": "054ebe7d23a97fe6c6959fc4a5017591",
+    "url": "/portfolio-with-react/static/media/envelope.054ebe7d.svg"
+  },
+  {
+    "revision": "f964a0164847ba900549",
+    "url": "/portfolio-with-react/static/js/runtime~main.f964a016.js"
+  },
+  {
+    "revision": "812274d9cad4d6585ba6",
+    "url": "/portfolio-with-react/static/js/main.812274d9.chunk.js"
+  },
+  {
+    "revision": "1b05be04ebb59718bb8c",
+    "url": "/portfolio-with-react/static/js/1.1b05be04.chunk.js"
+  },
+  {
+    "revision": "812274d9cad4d6585ba6",
+    "url": "/portfolio-with-react/static/css/main.3a2f9da6.chunk.css"
+  },
+  {
+    "revision": "1a1c04dcf0faf660cbdd9ec5a7e09b1f",
+    "url": "/portfolio-with-react/index.html"
+  }
+];
